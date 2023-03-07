560f891b531ef41584a233bca5f25b2bb25c1ec5
James Oubre <oubrejames@gmail.com>
2023-03-06 19:37:29 -0600

    Getting camera stream on computer from AUV

--- camera_stream.py
+++ camera_stream.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 BlueRov video capture class
-Copied from https://www.ardusub.com/developers/opencv.html
+Copied and modified from https://www.ardusub.com/developers/opencv.html
 """
 
 import cv2
@@ -142,27 +142,22 @@ class Video():
         self._new_frame = self.gst_to_opencv(sample)
 
         return Gst.FlowReturn.OK
+    
+    def start_stream(self):
+        print('Initialising stream...')
+        waited = 0
+        while not self.frame_available():
+            waited += 1
+            print('\r  Frame not available (x{})'.format(waited), end='')
+            cv2.waitKey(30)
+        print('\nSuccess!\nStarting streaming - press "q" to quit.')
 
-
-if __name__ == '__main__':
-    # Create the video object
-    # Add port= if is necessary to use a different one
-    video = Video()
-
-    print('Initialising stream...')
-    waited = 0
-    while not video.frame_available():
-        waited += 1
-        print('\r  Frame not available (x{})'.format(waited), end='')
-        cv2.waitKey(30)
-    print('\nSuccess!\nStarting streaming - press "q" to quit.')
-
-    while True:
-        # Wait for the next frame to become available
-        if video.frame_available():
-            # Only retrieve and display a frame if it's new
-            frame = video.frame()
-            cv2.imshow('frame', frame)
-        # Allow frame to display, and check if user wants to quit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        while True:
+            # Wait for the next frame to become available
+            if self.frame_available():
+                # Only retrieve and display a frame if it's new
+                frame = self.frame()
+                cv2.imshow('frame', frame)
+            # Allow frame to display, and check if user wants to quit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
--- test_class.py
+++ test_class.py
@@ -1,9 +1,77 @@
 from auv_move import HaloAUV
+import apriltag
+import argparse
+import cv2
+from camera_stream import Video
 
-auv = HaloAUV()
-# auv.restart_pixhawk()
-auv.arm()
-auv.set_relative_depth(250.35)
-auv.hold_depth()
-# while(1):
-#     auv.ascend(600)
+video = Video()
+
+video.start_stream()
+
+# Detect apriltags
+# in the input ima
+print("[INFO] detecting AprilTags...")
+options = apriltag.DetectorOptions(families="tag36h11")
+detector = apriltag.Detector(options)
+results = detector.detect(gray)
+print("[INFO] {} total AprilTags detected".format(len(results)))
+
+# # loop over the AprilTag detection results
+# for r in results:
+# 	# extract the bounding box (x, y)-coordinates for the AprilTag
+# 	# and convert each of the (x, y)-coordinate pairs to integers
+# 	(ptA, ptB, ptC, ptD) = r.corners
+# 	ptB = (int(ptB[0]), int(ptB[1]))
+# 	ptC = (int(ptC[0]), int(ptC[1]))
+# 	ptD = (int(ptD[0]), int(ptD[1]))
+# 	ptA = (int(ptA[0]), int(ptA[1]))
+# 	# draw the bounding box of the AprilTag detection
+# 	cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+# 	cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+# 	cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+# 	cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+# 	# draw the center (x, y)-coordinates of the AprilTag
+# 	(cX, cY) = (int(r.center[0]), int(r.center[1]))
+# 	cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+# 	# draw the tag family on the image
+# 	tagFamily = r.tag_family.decode("utf-8")
+# 	cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+# 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+# 	print("[INFO] tag family: {}".format(tagFamily))
+# # show the output image after AprilTag detection
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
+
+# # loop over the AprilTag detection results
+# for r in results:
+# 	# extract the bounding box (x, y)-coordinates for the AprilTag
+# 	# and convert each of the (x, y)-coordinate pairs to integers
+# 	(ptA, ptB, ptC, ptD) = r.corners
+# 	ptB = (int(ptB[0]), int(ptB[1]))
+# 	ptC = (int(ptC[0]), int(ptC[1]))
+# 	ptD = (int(ptD[0]), int(ptD[1]))
+# 	ptA = (int(ptA[0]), int(ptA[1]))
+# 	# draw the bounding box of the AprilTag detection
+# 	cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+# 	cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+# 	cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+# 	cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+# 	# draw the center (x, y)-coordinates of the AprilTag
+# 	(cX, cY) = (int(r.center[0]), int(r.center[1]))
+# 	cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+# 	# draw the tag family on the image
+# 	tagFamily = r.tag_family.decode("utf-8")
+# 	cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+# 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+# 	print("[INFO] tag family: {}".format(tagFamily))
+# # show the output image after AprilTag detection
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
+
+# auv = HaloAUV()
+# # auv.restart_pixhawk()
+# auv.arm()
+# auv.set_relative_depth(250.35)
+# auv.hold_depth()
+# # while(1):
+# #     auv.ascend(600)
